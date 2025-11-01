e3e959db3682d1683c7d6d94bbb355b7f2e85b4d
alis670 <asbhatti363@gmail.com>
2025-11-01 13:49:58 +0500

    Add initial script.js with login and chatbot functions

---
+++ script.js
@@ -0,0 +1,24 @@
+// Go to login page
+function goToLogin() {
+  window.location.href = "login.html";
+}
+
+// Open chatbot (Chatbase / custom later)
+function openChatbot() {
+  alert("Chatbot will open here soon 🤖");
+}
+
+// Firebase Placeholder
+/* 
+We will paste your Firebase config here later 👇
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
+*/
