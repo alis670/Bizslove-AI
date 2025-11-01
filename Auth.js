98473ccb08bae84b0b2f181bd8e26fad1ff2ac4c
alis670 <asbhatti363@gmail.com>
2025-11-01 21:33:36 +0500

    Fix email and password retrieval in Auth.js

--- Auth.js
+++ Auth.js
@@ -5,19 +5,19 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 window.loginUser = function () {
-  const email = email.value;
-  const password = password.value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
 
   signInWithEmailAndPassword(auth, email, password)
-  .then(() => window.location.href = "dashboard.html")
-  .catch(e => alert(e.message));
+    .then(() => window.location.href = "dashboard.html")
+    .catch(e => alert(e.message));
 };
 
 window.registerUser = function () {
-  const email = email.value;
-  const password = password.value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
 
   createUserWithEmailAndPassword(auth, email, password)
-  .then(() => alert("Account Created ✅ Login Now"))
-  .catch(e => alert(e.message));
+    .then(() => alert("Account Created ✅ Login Now"))
+    .catch(e => alert(e.message));
 };
